f7bac67ec39231775aa30bda10f0ba4fc2bbaf69
flevin58 <flevin58@gmail.com>
2024-10-26 22:38:05 +0200

    Removed gen.sh because the functionality is now in build.rs

--- gen.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if test "x$BINDGEN" == "x"; then
-    BINDGEN=bindgen
-fi
-$BINDGEN --no-layout-tests --allowlist-function "md5" -o src/md5_bindings.rs vendor/md5/md5.c 
-$BINDGEN --no-layout-tests --allowlist-function "sha256" -o src/sha256_bindings.rs vendor/sha256/sha256.h
